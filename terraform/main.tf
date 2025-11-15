232a60b8b8bbf074fabb6ffbae7436e118f7dbf1
Mahad Mohamed <tecknosap@gmail.com>
2025-11-15 11:29:01 +0000

    initail commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,3 @@
-provider "azurerm" {
-  features {}
-}
-
 resource "azurerm_resource_group" "rg" {
   name     = "rg-search-app"
   location = "East US"
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -10,4 +10,5 @@ terraform {
 
 provider "azurerm" {
   features {}
+  subscription_id = "45f251e8-d84d-4b8e-ac63-77eef4482127"
 }
