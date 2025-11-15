793a950a070f69615ab3542890ea63f477fb950b
Mahad Mohamed <tecknosap@gmail.com>
2025-11-15 11:52:51 +0000

    initail commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_service_plan" "plan" {
   name                = "search-app-plan"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  sku_name            = "F1"
+  sku_name            = "S1"
   os_type             = "Linux"
 }
 
