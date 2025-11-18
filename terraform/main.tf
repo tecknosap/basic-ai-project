cc359a8708c5236ec48e6c865dcd60a165439a00
Mahad Mohamed <tecknosap@gmail.com>
2025-11-18 17:58:58 +0000

    initial commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,62 +1,16 @@
-# provider "azurerm" {
-#   # Configuration options
-#   subscription_id = var.subscription_id
-#   features {
-    
-#   }
-# }
-
-# resource "azurerm_resource_group" "gocloudops" {
-#   name     = "gocloudops-resources"
-#   location = "West Europe"
-# }
-
-# resource "azurerm_app_service_plan" "gocloudops" {
-#   name                = "gocloudops-appserviceplan"
-#   location            = azurerm_resource_group.gocloudops.location
-#   resource_group_name = azurerm_resource_group.gocloudops.name
-
-#   sku {
-#     tier = "Standard"
-#     size = "S1"
-#   }
-# }
-
-# resource "azurerm_app_service" "gocloudops" {
-#   name                = "gocloudops-app-service"
-#   location            = azurerm_resource_group.gocloudops.location
-#   resource_group_name = azurerm_resource_group.gocloudops.name
-#   app_service_plan_id = azurerm_app_service_plan.gocloudops.id
-
-#   site_config {
-#     dotnet_framework_version = "v4.0"
-#     scm_type                 = "LocalGit"
-#   }
-
-#   app_settings = {
-#     "SOME_KEY" = "some-value"
-#   }
-
-#   connection_string {
-#     name  = "Database"
-#     type  = "SQLServer"
-#     value = "Server=some-server.mydomain.com;Integrated Security=SSPI"
-#   }
-# }
-
-
 provider "azurerm" {
+  # Configuration options
   subscription_id = var.subscription_id
-  features {}
+  features {
+    
+  }
 }
 
-# Resource Group
 resource "azurerm_resource_group" "gocloudops" {
   name     = "gocloudops-resources"
   location = "West Europe"
 }
 
-# App Service Plan
 resource "azurerm_app_service_plan" "gocloudops" {
   name                = "gocloudops-appserviceplan"
   location            = azurerm_resource_group.gocloudops.location
@@ -66,41 +20,8 @@ resource "azurerm_app_service_plan" "gocloudops" {
     tier = "Standard"
     size = "S1"
   }
-  kind     = "Linux"     # Explicitly set to Linux
-  reserved = true        # Required for Linux
-
 }
 
-# Azure OpenAI
-resource "azurerm_cognitive_account" "openai" {
-  name                = "gocloudops-a-openai"
-  location            = azurerm_resource_group.gocloudops.location
-  resource_group_name = azurerm_resource_group.gocloudops.name
-  kind                = "OpenAI"
-  sku_name            = "S0"
-  custom_subdomain_name = "gocloudops-a-openai"
-  
-}
-
-resource "azurerm_cognitive_deployment" "gpt4omini" {
-  name                 = "gpt4omini"
-  cognitive_account_id = azurerm_cognitive_account.openai.id
-
-  model {
-    format  = "OpenAI"
-    name    = "gpt-4o-mini"
-    version = "2024-08-06"
-  }
-
-  sku {
-    name     = "Standard"
-    capacity = 1
-  }
-
-
-}
-
-# App Service (Python)
 resource "azurerm_app_service" "gocloudops" {
   name                = "gocloudops-app-service"
   location            = azurerm_resource_group.gocloudops.location
@@ -108,16 +29,95 @@ resource "azurerm_app_service" "gocloudops" {
   app_service_plan_id = azurerm_app_service_plan.gocloudops.id
 
   site_config {
-    linux_fx_version = "PYTHON|3.11"
-    scm_type       = "LocalGit"
+    dotnet_framework_version = "v4.0"
+    scm_type                 = "LocalGit"
   }
 
   app_settings = {
-    "AZURE_OPENAI_ENDPOINT"    = azurerm_cognitive_account.openai.endpoint
-    "AZURE_OPENAI_API_KEY"     = azurerm_cognitive_account.openai.primary_access_key
-    "AZURE_OPENAI_DEPLOYMENT"  = azurerm_cognitive_deployment.gpt4omini.name
+    "SOME_KEY" = "some-value"
+  }
+
+  connection_string {
+    name  = "Database"
+    type  = "SQLServer"
+    value = "Server=some-server.mydomain.com;Integrated Security=SSPI"
   }
 }
+
+
+# provider "azurerm" {
+#   subscription_id = var.subscription_id
+#   features {}
+# }
+
+# # Resource Group
+# resource "azurerm_resource_group" "gocloudops" {
+#   name     = "gocloudops-resources"
+#   location = "West Europe"
+# }
+
+# # App Service Plan
+# resource "azurerm_app_service_plan" "gocloudops" {
+#   name                = "gocloudops-appserviceplan"
+#   location            = azurerm_resource_group.gocloudops.location
+#   resource_group_name = azurerm_resource_group.gocloudops.name
+
+#   sku {
+#     tier = "Standard"
+#     size = "S1"
+#   }
+#   kind     = "Linux"     # Explicitly set to Linux
+#   reserved = true        # Required for Linux
+
+# }
+
+# # Azure OpenAI
+# resource "azurerm_cognitive_account" "openai" {
+#   name                = "gocloudops-a-openai"
+#   location            = azurerm_resource_group.gocloudops.location
+#   resource_group_name = azurerm_resource_group.gocloudops.name
+#   kind                = "OpenAI"
+#   sku_name            = "S0"
+#   custom_subdomain_name = "gocloudops-a-openai"
+  
+# }
+
+# resource "azurerm_cognitive_deployment" "gpt4omini" {
+#   name                 = "gpt4omini"
+#   cognitive_account_id = azurerm_cognitive_account.openai.id
+
+#   model {
+#     format  = "OpenAI"
+#     name    = "gpt-4o-mini"
+#     version = "2024-08-06"
+#   }
+
+#   sku {
+#     name     = "Standard"
+#     capacity = 1
+#   }
+
+
+# }
+
+# # App Service (Python)
+# resource "azurerm_app_service" "gocloudops" {
+#   name                = "gocloudops-app-service"
+#   location            = azurerm_resource_group.gocloudops.location
+#   resource_group_name = azurerm_resource_group.gocloudops.name
+#   app_service_plan_id = azurerm_app_service_plan.gocloudops.id
+
+#   site_config {
+#     linux_fx_version = "PYTHON|3.11"
+#     scm_type       = "LocalGit"
+#   }
+
+#   app_settings = {
+#     "AZURE_OPENAI_ENDPOINT"    = azurerm_cognitive_account.openai.endpoint
+#     "AZURE_OPENAI_API_KEY"     = azurerm_cognitive_account.openai.primary_access_key
+#     "AZURE_OPENAI_DEPLOYMENT"  = azurerm_cognitive_deployment.gpt4omini.name
+#   }
+# }
 
 
 
